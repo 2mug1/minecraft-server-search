0d4dffef678e6060a92c8d98dcf687b374cb654d
takagi <iam.takagi.dev@gmail.com>
2022-02-20 20:23:47 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 const IndexPage: React.FC<{}> = () => (
     <div className="mt-5">
-        <p>マイクラのサーバーを検索するやつです<br/>
+        <p>マイクラのサーバーを検索できるサービスです。<br/>
         ソースコードは <a href="https://github.com/iamtakagi/minecraft-server-search" className="border-b hover:border-purple-800 text-purple-800 border-transparent">こちら</a> に置いています。</p>
     </div>
 )
